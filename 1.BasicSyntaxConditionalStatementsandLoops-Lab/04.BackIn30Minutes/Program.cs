28ad6f90378c6ccf32b200db67b66ca04d9f8f59
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-04 00:33:31 +0300

    Add files via upload

---
+++ 1.BasicSyntaxConditionalStatementsandLoops-Lab/04.BackIn30Minutes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.BackIn30Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int inputhours =int.Parse(Console.ReadLine()) ;
+           int inputminutes =int.Parse(Console.ReadLine());
+
+            int minutesPlus15 = inputminutes + 30;
+            int hours = inputhours + minutesPlus15 / 60;
+            int minutes = minutesPlus15 % 60;
+
+            if (hours == 24)
+                hours = 0;
+            if (minutes < 10)
+                Console.WriteLine($"{hours}:0{minutes}");
+            else
+                Console.WriteLine($"{hours}:{minutes}");
+
+        }
+    }
+}

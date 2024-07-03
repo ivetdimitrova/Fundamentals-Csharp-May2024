8cef9b63d510d14f3a44f2e48d61a3c65e1fffe0
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-04 00:36:56 +0300

    Add files via upload

---
+++ 1.BasicSyntaxConditionalStatementsandLoops-Lab/09.SumofOddNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _09.SumofOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfOddNumbers = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int oddNumber = 1;
+            for(int i = 0; i < countOfOddNumbers; i++)
+            {
+                sum += oddNumber;
+                Console.WriteLine(oddNumber);
+                oddNumber += 2;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}

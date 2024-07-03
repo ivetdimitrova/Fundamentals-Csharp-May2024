f9e2aa0e24509fdd368e4fcae8d1ca8aa0a3f421
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-04 00:37:50 +0300

    Add files via upload

---
+++ 1.BasicSyntaxConditionalStatementsandLoops-Lab/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace _11.MultiplicationTable2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if(secondNumber <= 10)
+            {
+                for(int i = secondNumber; i <= 10;i++)
+                {
+                    Console.WriteLine($"{firstNumber} X {i} = {firstNumber * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{firstNumber} X {secondNumber} = {firstNumber * secondNumber}");
+            }
+        }
+    }
+}

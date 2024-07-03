8f15d594fe2fbbb7af4749608941e70abba5ac99
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-04 00:32:28 +0300

    Add files via upload

---
+++ 1.BasicSyntaxConditionalStatementsandLoops-Lab/03.PassedOrFailed/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _03.PassedOrFailed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade > 2.99)
+                {
+                    Console.WriteLine("Passed!");
+                }
+                else
+                {
+                    Console.WriteLine("Failed!");
+                }
+
+            }
+        }
+
+    }
+
+}

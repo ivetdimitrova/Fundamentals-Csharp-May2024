f9f104cab743a935349c048149a876a165498b65
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:44:49 +0300

    Add

---
+++ 4.Methods-Lab/09.GreaterОfTwoValues/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+namespace _09.GreaterОfTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            switch (type)
+            {
+                case "int":
+                    IntegerType();
+                    break;
+                case "char":
+                    CharType();
+                    break;
+                case "string":
+                    StringType();
+                    break;
+            }
+            
+        }
+
+        static void IntegerType()
+        {
+            int value1 = int.Parse(Console.ReadLine());
+            int value2 = int.Parse(Console.ReadLine());
+
+            if (value1 > value2)
+            {
+                Console.WriteLine(value1);
+            }
+            else
+            {
+                Console.WriteLine(value2);
+            }
+        }
+        static void CharType ()
+        { 
+            char value1 = char.Parse(Console.ReadLine());
+            char value2 = char.Parse(Console.ReadLine());
+
+            if ((int)value1 > (int)value2)
+            {
+                Console.WriteLine(value1);
+            }
+            else
+            {
+                Console.WriteLine(value2);
+            }
+        }
+        static void StringType()
+        {
+            string value1 = Console.ReadLine();
+            string value2 = Console.ReadLine();
+
+            //int end = 0;
+            //if (value2.Length > value1.Length)
+            //{
+            //    end = (int)value2.Length;
+            //}
+            //else
+            //{
+            //    end = (int)value1.Length;
+            //}
+
+            //for (int i = 0; i < end; i++)
+            //{
+            //    char symbol1 = value1[i];
+            //    char symbol2 = value2[i];
+
+            //    if ((int)symbol1 > (int)symbol2)
+            //    {
+            //        Console.Write(symbol1);
+            //    }
+            //    else
+            //    {
+            //        Console.Write(symbol2);
+            //    }
+            string result = string.Empty;
+            int comparison = value1.CompareTo(value2);
+            if (comparison > 0)
+            {
+                result = value1;
+            }
+            else
+            {
+                result = value2;
+            }
+            Console.WriteLine(result);
+        }
+            
+        }
+    }
+
---
+++ 4.Methods-Lab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int evenSum = 0;
+            while (number != 0)
+            {
+                int digit = Math.Abs(number % 10);
+                if (digit % 2 == 0)
+                {
+                    evenSum += digit;
+                }
+                number /= 10;
+            }
+            return evenSum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int OddSum = 0;
+            while (number != 0)
+            {
+                int digit = Math.Abs(number % 10);
+                if (digit % 2 != 0)
+                {
+                    OddSum += digit;
+                }
+                number /= 10;
+            }
+            return OddSum;
+        }
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int evenSum = GetSumOfEvenDigits(number);
+            int oddSum = GetSumOfOddDigits(number);
+            return evenSum * oddSum;
+        }
+        
+        
+    }
+}

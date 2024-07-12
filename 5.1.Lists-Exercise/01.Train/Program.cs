8fd274a7f3612689f7fc008e608c5503474ddbf7
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:59:04 +0300

    Add

---
+++ 5.1.Lists-Exercise/01.Train/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] command = input.Split();
+
+                if (command[0] == "Add")
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    AddPeople(wagons, int.Parse(command[0]),maxCapacity);
+                }
+
+            }
+            Console.WriteLine(String.Join(" ", wagons));
+        }
+
+         static void AddPeople(List<int> wagons, int value,int capacity)
+        {
+            for (int i = 0; i < wagons.Count; i++)
+            {
+                if (wagons[i] + value <= capacity)
+                {
+                    wagons[i] += value;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 5.1.Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] command = input.Split();
+                switch (command[0])
+                {
+                    case "Delete":
+                        DeleteAllElements(numbers,int.Parse( command[1]));
+                        break;
+                    case "Insert":
+                        numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                        break;
+                }
+                
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        static void DeleteAllElements(List<int> numbers,int number)
+        {
+            if (numbers.Contains(number))
+            {
+                for (int i =0; i< numbers.Count;i++)
+                {
+                    if (numbers[i] == number)
+                    {
+                        numbers.Remove(number);
+                    }
+
+                }
+            }
+        }
+    }
+}
+

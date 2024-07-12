babfc974a0d841b381463c30505b8a9d98999f7d
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:37:35 +0300

    Add files via upload

---
+++ 4.Methods-Lab/01.SignОfIntegerNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Channels;
+
+namespace _01.SignОfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            IsPositive(number);
+            IsNegative(number);
+            IsTheNumberZero(number);
+            void IsPositive(int number)
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine($"The number {number} is positive.");
+                }
+            }
+
+            void IsNegative(int number)
+            {
+                if (number < 0)
+                {
+                    Console.WriteLine($"The number {number} is negative.");
+                }
+            }
+
+            void IsTheNumberZero(int number)
+            {
+                if (number == 0)
+                {
+                    Console.WriteLine($"The number {number} is zero.");
+                }
+
+            }
+        }
+    }
+}
---
+++ 4.Methods-Lab/02.Grades/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Channels;
+
+namespace _02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            IsFail(grade);
+            IsPoor(grade);
+            IsGood(grade);
+            IsVeryGood(grade);
+            IsExcellent(grade);
+            void IsFail(double mark)
+            {
+                if (mark <= 2.99)
+                {
+                    Console.WriteLine($"Fail");
+                }
+            }
+
+            void IsPoor(double mark)
+            {
+                if (mark>= 3.00 && mark <= 3.49)
+                {
+                    Console.WriteLine($"Poor");
+                }
+            }
+
+            void IsGood(double mark)
+            {
+                if (mark>=3.50&&mark <= 4.49)
+                {
+                    Console.WriteLine($"Good");
+                }
+            }
+
+            void IsVeryGood(double mark)
+            {
+                if (mark >=4.50 && mark <= 5.49)
+                {
+                    Console.WriteLine($"Very good");
+                }
+            }
+
+            void IsExcellent(double mark)
+            {
+                if (mark >= 5.50 && mark <= 6.00)
+                {
+                    Console.WriteLine($"Excellent");
+                }
+            }
+        }
+    }
+}

14d1dd0a33fe62fbf895460abbccae25711baad2
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 18:58:38 +0300

    Add files via upload

---
+++ 2.DataTypesandVariables-Lab/08.TownInfo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _08.TownInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            double kilometers = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {townName} has population of {population} and area {kilometers} square km.");
+        }
+    }
+}
---
+++ 2.DataTypesandVariables-Lab/09.CharsToString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            for (int i = 0; i < 3; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                text += symbol;
+            }
+
+            Console.WriteLine(text);
+
+        }
+    }
+}

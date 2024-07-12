923c72f7db1b151990329efee176a66f341ca800
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:22:04 +0300

    Add

---
+++ 7.1.AssociativeArrays-Exercise/04.SoftUniParking/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+namespace _04.SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> parking = new Dictionary<string,string>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                switch (command[0])
+                {
+                    case "register":
+                        Register(parking, command[1], command[2]);
+                        break;
+                    case "unregister":
+                        Unregister(parking, command[1]);
+                        break;
+                }
+            }
+
+            Print(parking);
+        }
+
+        private static void Print(Dictionary<string, string> parking)
+        {
+            foreach (var user in parking)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+
+        private static void Unregister(Dictionary<string, string> parking, string username)
+        {
+            if (!parking.ContainsKey(username))
+            {
+                Console.WriteLine($"ERROR: user {username} not found");
+            }
+            else
+            {
+                Console.WriteLine($"{username} unregistered successfully");
+                parking.Remove(username);
+            }
+        }
+
+        private static void Register(Dictionary<string, string> parking, string username, string licensePlateNumber)
+        {
+            if (!parking.ContainsKey(username))
+            {
+                Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                parking.Add(username,licensePlateNumber);
+            }
+            else
+            {
+                Console.WriteLine($"ERROR: already registered with plate number {parking[username]}");
+            }
+        }
+    }
+}
---
+++ 7.1.AssociativeArrays-Exercise/05.Courses/Program.cs
@@ -0,0 +1,67 @@
+﻿
+
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+namespace _05.Courses
+{
+    public class Course
+    {
+        public Course(string name)
+        {
+            Students = new List<string>();
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public List<string> Students { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"{Name}: {Students.Count}\n";
+
+            for (int i = 0; i < Students.Count; i++)
+            {
+                result += $"--{Students[i]}\n";
+            }
+
+            return result.Trim();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Course> course = new List<Course>();
+            Dictionary<string,Course> courses = new Dictionary<string,Course>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] information = input.Split(":").ToArray();
+
+                TakeInformation(information, courses);
+            }
+
+            foreach (KeyValuePair<string, Course> pair in courses)
+            {
+                Console.WriteLine(pair.Value);
+            }
+        }
+
+
+        private static void TakeInformation(string[] information, Dictionary<string, Course> courses)
+        {
+            string courseName = information[0].Trim();
+            string studentName = information[1];
+
+            if (!courses.ContainsKey(courseName))
+            {
+                Course course = new Course(courseName);
+                courses.Add(courseName, course);
+            }
+
+            courses[courseName].Students.Add(studentName);
+        }
+    }
+}

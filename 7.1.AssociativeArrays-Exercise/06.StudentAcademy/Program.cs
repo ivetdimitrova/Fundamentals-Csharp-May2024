3b143f8072a4f0fe354ae49c3f7380eb18342f09
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:23:00 +0300

    Add

---
+++ 7.1.AssociativeArrays-Exercise/06.StudentAcademy/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string,double> students = new Dictionary<string,double>();
+           Dictionary<string,int> times = new Dictionary<string,int>();
+
+           int count = int.Parse(Console.ReadLine());
+
+           for (int i = 0; i < count; i++)
+           {
+               string name = Console.ReadLine();
+               double grade = double.Parse(Console.ReadLine());
+
+               if (!students.ContainsKey(name))
+               {
+                   students.Add(name,0);
+               }
+
+               if (!times.ContainsKey(name))
+               {
+                   times.Add(name, 0);
+               }
+
+               times[name]++;
+               students[name] += grade;
+           }
+
+           foreach (var student in students)
+           {
+               string name = student.Key;
+               double grade = student.Value;
+
+               foreach (var time in times)
+               {
+                   if (time.Key == name)
+                   {
+                       double averageGrade = grade / time.Value;
+                       if (averageGrade >= 4.50)
+                       {
+                           Console.WriteLine($"{name} -> {averageGrade:f2}");
+                       }
+                   }
+               }
+           }
+        }
+    }
+}
---
+++ 7.1.AssociativeArrays-Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _07.CompanyUsers
+{
+    public class Company
+    {
+        public Company(string name)
+        {
+            EmployeeIds = new List<string>();
+            Name = name;
+        }
+        public string Name { get; set; }
+
+        public List<string> EmployeeIds { get; set; }
+
+        public void AddEmployee(string employeeId)
+        {
+            if (!EmployeeIds.Contains(employeeId))
+            {
+                EmployeeIds.Add(employeeId);
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = $"{Name}\n";
+
+            for (int i = 0; i < EmployeeIds.Count; i++)
+            {
+                result += $"-- {EmployeeIds[i]}\n";
+            }
+
+            return result.Trim();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Company> companies = new Dictionary<string, Company>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] information = input.Split("->").ToArray();
+
+                string name = information[0].Trim();
+                string id = information[1].Trim();
+
+
+
+                if (!companies.ContainsKey(name))
+                {
+                    Company company = new Company(name);
+                    companies.Add(name, company);
+                }
+
+                companies[name].AddEmployee(id);
+            }
+
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Value);
+            }
+        }
+    }
+}

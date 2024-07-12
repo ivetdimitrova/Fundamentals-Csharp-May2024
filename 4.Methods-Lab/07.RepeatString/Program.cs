2fdc3cf88e72e0f8e95cf16dd66eaf519eeea6d9
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:44:00 +0300

    Add

---
+++ 4.Methods-Lab/07.RepeatString/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbols = Console.ReadLine();
+            int times = int.Parse(Console.ReadLine());
+            string result = RepeatString(symbols, times);
+
+            Console.WriteLine(result);
+        }
+
+        static string RepeatString(string str, int times)
+        {
+            string result = " ";
+            for (int i = 0; i < times; i++)
+            {
+                result += str;
+
+            }
+            return result;
+        }
+    }
+}
---
+++ 4.Methods-Lab/08.MathPower/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int basis = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = Result(basis, power);
+            Console.WriteLine(result);
+        }
+
+        static double Result(int value, int power)
+        {
+            
+            return Math.Pow(value, power); ;
+        }
+    }
+}

9bde7732285b0f361793b13aad59de25f6c054e9
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:16:55 +0300

    Add

---
+++ 7.AssociativeArrays-Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double,int> counts = new SortedDictionary<double,int>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}

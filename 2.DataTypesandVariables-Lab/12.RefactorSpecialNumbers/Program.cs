43899b7255db8fdd56cba8626b863ad839225810
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:04:29 +0300

    Add files via upload

---
+++ 2.DataTypesandVariables-Lab/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _12.RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int number = 0;
+            bool isSpecialNumber = false;
+            for (int i = 1; i <= count; i++)
+            {
+                number = i;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number = number / 10;
+                }
+                isSpecialNumber = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", i, isSpecialNumber);
+                sum = 0;
+               
+            }
+        }
+    }
+}

fc89680af69f28d06cbff9c01379ddcd11b715e1
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:09:40 +0300

    Add files via upload

---
+++ 2.1.DataTypesAndVariables-Exercise/03.Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03.Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+           decimal courses = Math.Ceiling((decimal)countOfPeople / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
---
+++ 2.1.DataTypesAndVariables-Exercise/04.SumOfChars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace _04.SumOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                sum += (int)symbol;
+            }
+            Console.WriteLine("The sum equals: " + sum);
+        }
+    }
+}

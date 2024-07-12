835838656abbf4fe9abd5d4687f31702e38ead89
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:15:30 +0300

    Add files via upload

---
+++ 3.Arrays-Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letter = Console.ReadLine().Split(" ").ToArray();
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                Console.Write(letter[letter.Length - 1 - i] + " ");
+            }
+        }
+    }
+}

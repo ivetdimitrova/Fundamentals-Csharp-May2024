df37d79eaf628110a61b91fbba669ff9ce140383
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:08:11 +0300

    Add files via upload

---
+++ 2.1.DataTypesAndVariables-Exercise/01.IntegerOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.IntegerOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int firstNUmber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int firstOperation = firstNUmber + secondNumber;
+            int secondOperation = firstOperation / thirdNumber;
+            int thirdOperation = secondOperation * fourthNumber;
+
+            Console.WriteLine(thirdOperation);
+        }
+    }
+}

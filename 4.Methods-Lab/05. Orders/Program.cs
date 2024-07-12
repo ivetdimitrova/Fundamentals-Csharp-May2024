9651fb37f7a7207c1e2d86a84da022bfc05c3e48
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:42:27 +0300

    Add

---
+++ 4.Methods-Lab/05. Orders/Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            IsCoffee(product, 1.50,count);
+            IsWater(product,1.00,count);
+            IsCoke(product,1.40,count);
+            IsSnacks(product,2.00,count);
+             void IsCoffee(string product, double price, int count)
+            {
+                if (product == "coffee")
+                {
+                    double allPrice = price * count;
+                    Console.WriteLine($"{allPrice:f2}");
+                }
+            }
+
+            void IsWater(string product, double price, int count)
+            {
+                if (product == "water")
+                {
+                    double allPrice = price * count;
+                    Console.WriteLine($"{allPrice:f2}");
+                }
+            }
+
+            void IsCoke(string product, double price, int count)
+            {
+                if (product == "coke")
+                {
+                    double allPrice = price * count;
+                    Console.WriteLine($"{allPrice:f2}");
+                }
+            }
+
+            void IsSnacks(string product, double price, int count)
+            {
+                if (product == "snacks")
+                {
+                    double allPrice = price * count;
+                    Console.WriteLine($"{allPrice:f2}");
+                }
+            }
+        }
+
+        
+    }
+}

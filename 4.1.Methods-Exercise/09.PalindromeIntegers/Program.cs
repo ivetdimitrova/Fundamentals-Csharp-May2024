46da11b296413ca4948daa71cac3b3f0e11af25f
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:51:36 +0300

    Add

---
+++ 4.1.Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string numStr;
+
+            while ((numStr = Console.ReadLine()) != "END")
+            {
+                Console.WriteLine(IsPalindrome(numStr));
+            }
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            string first = input.Substring(0, input.Length / 2);
+            char[] arr = input.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
+    }
+}

1f1684be4b0837e58d3c1ed107927a4642c021b5
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:47:01 +0300

    Add

---
+++ 4.1.Methods-Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string sentence = Console.ReadLine();
+
+            int vowelsCount = GetVowelsCount(sentence);
+
+            Console.WriteLine(vowelsCount);
+        }
+
+        private static int GetVowelsCount(string sentence)
+        {
+            int count = 0;
+
+            sentence = sentence.ToLower();
+            foreach (char symbol in sentence)
+            {
+                switch (symbol)
+                {
+                    case 'e':
+                    case 'o':
+                    case 'a':
+                    case 'i':
+                    case 'u':
+                    count++;
+                    break;
+                }
+            }
+            return count;
+        }
+
+            
+        
+    }
+}

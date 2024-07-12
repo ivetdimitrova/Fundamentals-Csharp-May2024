2427daab4c73066e15f824c0e39ef02a2592ce8b
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:21:45 +0300

    Add files via upload

---
+++ 3.1.Arrays-Exercisе/06.EqualSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06.EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int index = -1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                index = -1;
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    leftSum += numbers[j];
+                }
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    rightSum += numbers[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}

6695a0221401b4d204ac586b1ad2c8a9dbfb622a
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:08:59 +0300

    Add

---
+++ 6.ObjectsandClasses-Lab/06.StoreBoxes/Program.cs
@@ -0,0 +1,81 @@
+﻿
+using System.Threading.Channels;
+
+namespace _06.StoreBoxes
+{
+    class Item
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    class Box
+    {
+        public Box()
+        {
+            Item = new Item();
+        }
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal PriceBox { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+               // Box box = new Box();
+                string[] information = input.Split().ToArray();
+                TakeBoxInformation(boxes, information);
+                
+            }
+            boxes.Sort(((box, box1) => box1.PriceBox.CompareTo(box.PriceBox) ));
+            foreach (Box box in boxes)
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price :f2}: {box.Quantity}");
+                Console.WriteLine($"-- ${box.PriceBox :f2}");
+            }
+        }
+
+        private static void TakeBoxInformation(List<Box> boxes, string[] information)
+        {
+           string serialNUmber = information[0];
+           string itemName = information[1];
+           int quantity = int.Parse(information[2]);
+           decimal itemPrice = decimal.Parse(information[3]);
+           decimal boxPrice = quantity * itemPrice;
+
+           Box box = new Box();
+           box.Item = new Item();
+           
+           box.SerialNumber = serialNUmber;
+           box.Item.Name = itemName;
+           box.Quantity = quantity;
+           box.Item.Price = itemPrice;
+           box.PriceBox = boxPrice;
+
+           boxes.Add(box);
+            //decimal max = Decimal.MinValue;
+            //int count = 0;
+            //if (boxPrice > max)
+            //{
+            //     boxes.Insert(count,box);
+            //}
+            //else
+            //{
+            //    boxes.Add(box);
+            //    count++;
+        }
+
+
+    }
+}
+
---
+++ 6.ObjectsandClasses-Lab/07.VehicleCatalogue/Program.cs
@@ -0,0 +1,113 @@
+﻿
+
+
+
+namespace _07.VehicleCatalogue
+{
+    class Truck
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Weight { get; set; }
+        public Truck(string brand, string model, int weight)
+        {
+            Brand = brand;
+            Model = model;
+            Weight = weight;
+        }
+    }
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public Car(string brand, string model, int horsePower)
+        {
+            Brand = brand;
+            Model = model;
+            HorsePower = horsePower;
+        }
+    }
+
+    class Catalog
+    {
+        public Catalog()
+        {
+            Trucks = new List<Truck>();
+            Cars = new List<Car>();
+        }
+        public List<Truck> Trucks { get; set; }
+        public List<Car> Cars { get; set; }
+
+        public void AddCar(Car car)
+        {
+            Cars.Add(car);
+        }
+
+        public void AddTruck(Truck truck)
+        {
+            Trucks.Add(truck);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalog catalog = new Catalog();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] information = input.Split("/").ToArray();
+
+                switch (information[0])
+                {
+                    case "Car":
+                        TakeCarInfo(information, catalog);
+                        break;
+                    case "Truck":
+                        TakeTruckInfo(information, catalog);
+                        break;
+                }
+                
+            }
+            Console.WriteLine("Cars:");
+            foreach (var car in catalog.Cars.OrderBy(c => c.Brand))
+            {
+                Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+            }
+            Console.WriteLine("Trucks:");
+
+            foreach (var truck in catalog.Trucks.OrderBy(t => t.Brand))
+            {
+                Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+
+            }
+        }
+
+
+
+        private static void TakeTruckInfo(string[] information, Catalog catalog)
+        {
+            string brand = information[1];
+            string model = information[2];
+            int weight = int.Parse(information[3]);
+
+            Truck truck = new Truck(brand,model,weight);
+
+            catalog.AddTruck(truck);
+        }
+
+        private static void TakeCarInfo(string[] information, Catalog catalog)
+        {
+            string brand = information[1];
+            string model = information[2];
+            int horsePower = int.Parse(information[3]);
+
+            Car car = new Car(brand,model,horsePower);
+
+            catalog.AddCar(car);
+        }
+    }
+}

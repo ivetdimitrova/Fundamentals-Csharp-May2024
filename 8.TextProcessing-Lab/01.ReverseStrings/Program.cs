b9b04160b655aa04c80ecf518b1ac827ccadd57d
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:25:25 +0300

    Add

---
+++ 8.TextProcessing-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> words = new Dictionary<string, string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string reversedWord = "";
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversedWord += input[i];
+                }
+
+                words.Add(input,reversedWord);
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} = {word.Value}");
+            }
+        }
+    }
+}
---
+++ 8.TextProcessing-Lab/02.RepeatStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace _02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            StringBuilder concatenatedString = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    concatenatedString.Append(word);
+                }
+            }
+
+            Console.WriteLine(concatenatedString);
+        }
+    }
+}

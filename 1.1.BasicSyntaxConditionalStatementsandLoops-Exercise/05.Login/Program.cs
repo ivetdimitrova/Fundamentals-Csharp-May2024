d5a5ad2f8295f0ee35a8fe004a84fc5ddc181225
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 18:45:03 +0300

    Add files via upload

---
+++ 1.1.BasicSyntaxConditionalStatementsandLoops-Exercise/05.Login/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _05.Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string username = Console.ReadLine();
+
+            char[] stringArray = username.ToCharArray();
+            Array.Reverse(stringArray);
+            string password = new string(stringArray);
+
+            int atempts = 4;
+            while (true)
+            {
+                string tryPassword = Console.ReadLine();
+                atempts--;
+                if (atempts == 0)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                else
+                {
+                    if (tryPassword == password)
+                    {
+                        Console.WriteLine($"User {username} logged in.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect password. Try again.");
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 1.1.BasicSyntaxConditionalStatementsandLoops-Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _06.StrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+
+            int allnumber = number;
+            int factorialSum = 0;
+           while (number > 0)
+            {
+                int endNum = number % 10;
+                number /= 10;
+            int factorialSum1 = 1;
+                for(int i = 1; i <= endNum; i++)
+                {
+                    factorialSum1 *= i;
+                }
+                factorialSum += factorialSum1;
+            }
+            string isStrongNumber = factorialSum == allnumber ? "yes" : "no";
+            Console.WriteLine(isStrongNumber);
+           
+        }
+    }
+}

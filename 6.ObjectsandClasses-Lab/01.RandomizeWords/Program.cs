3985663831f2d7d7802e84463b557ec3b6f6e97f
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:06:04 +0300

    Add

---
+++ 6.ObjectsandClasses-Lab/01.RandomizeWords/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<string> separated = input.Split().ToList();
+            List<string> randomizeWords = new List<string>();
+            Random random = new Random();
+            for (int i = 0; i < separated.Count; i+= 0)
+            {
+                int randomIndex = random.Next(0, separated.Count);
+                string value = separated[randomIndex];
+                randomizeWords.Add(value);
+                separated.RemoveAt(randomIndex);
+                
+            }
+
+            foreach (string element in randomizeWords)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ 6.ObjectsandClasses-Lab/03.Songs/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _03.Songs
+{
+    class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 1; i <= count; i++)
+            {
+                string[] data = Console.ReadLine().Split("_").ToArray();
+
+                string typeList = data[0];
+                string name = data[1];
+                string time = data[2];
+
+                Song song = new Song();
+                song.TypeList =typeList;
+                song.Name =name;
+                song.Time =time;
+            }
+
+            string favoriteList = Console.ReadLine();
+
+            if (favoriteList == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == favoriteList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+}

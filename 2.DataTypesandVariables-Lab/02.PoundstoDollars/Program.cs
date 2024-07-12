8225ad50d5395d1caf3b70aaf345f795cfdfaa6d
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 18:51:34 +0300

    Add files via upload

---
+++ 2.DataTypesandVariables-Lab/02.PoundstoDollars/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02.PoundstoDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pounds = int.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31m;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}

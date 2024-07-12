7b80d32c23243f380e1afae28bf9cca363f97f73
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:19:26 +0300

    Add files via upload

---
+++ 3.1.Arrays-Exercisе/01.Train/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int wagons = int.Parse(Console.ReadLine());
+
+            int[] passengers = new int[wagons];
+
+            int allPassengers = 0;
+            for (int i = 0; i < wagons; i++)
+            {
+                passengers[i] = int.Parse(Console.ReadLine());
+                allPassengers += passengers[i];
+            }
+            Console.WriteLine(String.Join(" ", passengers));
+             Console.WriteLine(allPassengers);
+
+
+        }
+    }
+}
---
+++ 3.1.Arrays-Exercisе/02.CommonElements/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split().ToArray();
+            string[] arr2 = Console.ReadLine().Split().ToArray();
+
+            string[] commonElements = new string[arr2.Length];
+            int index = 0;
+
+            // Итерираме през втория масив
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = 0; j < arr1.Length; j++)
+                {
+                    if (arr2[i] == arr1[j])
+                    {
+                        commonElements[index++] = arr2[i];
+                        break;  // Спираме вътрешния цикъл след намиране на съвпадение
+                    }
+                }
+            }
+
+            // Отпечатваме общите елементи
+            if (index > 0)
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    Console.Write(commonElements[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("No common elements found");
+            }
+
+        }
+    }
+}

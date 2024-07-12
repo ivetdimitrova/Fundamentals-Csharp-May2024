e0ffbc366e906b16d72ebe639578dc2d9226b348
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:10:57 +0300

    Add files via upload

---
+++ 2.1.DataTypesAndVariables-Exercise/05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05.PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+            char symbol= (char)i;
+            Console.Write($"{symbol} ");
+            }
+
+        }
+    }
+}

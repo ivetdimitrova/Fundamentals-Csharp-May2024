ceb6f974f60e7a17adfe85faa1e3be7bf64ac94f
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:17:39 +0300

    Add files via upload

---
+++ 3.Arrays-Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] array2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int sum = 0;
+            int difference = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (difference == 0 && array[i] != array2[i])
+                {
+                    difference++;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                }
+                else
+                {
+                    sum += array[i];
+                }
+
+            }
+            if (difference == 0)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}

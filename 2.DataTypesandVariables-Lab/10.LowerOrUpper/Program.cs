ace4b455f135fd20fc6a2f2e5dc0978d54cffc9e
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:02:03 +0300

    Add files via upload

---
+++ 2.DataTypesandVariables-Lab/10.LowerOrUpper/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _10.LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if (letter >= 'A' && letter <= 'Z')
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if(letter >= 'a' && letter <='z')
+            {
+                Console.WriteLine("lower-case");
+            }
+
+        }
+    }
+}
---
+++ 2.DataTypesandVariables-Lab/11.RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _11.RefactorVolumeOfPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+            double volume = Math.Pow(3, -1) * (length*width*height);
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+        }
+    }
+}

ab6c5b79a3d6ade15d4a66e4de614d74d6daf9fa
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:11:43 +0300

    Add files via upload

---
+++ 2.1.DataTypesAndVariables-Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int capacity = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                capacity+= liters;
+                if(capacity >255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    capacity -= liters;
+                    continue;
+                }
+
+            }
+            Console.WriteLine(capacity);
+        }
+    }
+}
---
+++ 2.1.DataTypesAndVariables-Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+
+            string nameOfBigger = "";
+
+            double max = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                string mobel = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radius, 2)*height;
+                if(volume > max)
+                {
+                    nameOfBigger = mobel;
+                    max = volume;
+
+                }
+            }
+            Console.WriteLine(nameOfBigger);
+        }
+    }
+}

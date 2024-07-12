5bb2b97a8d1028742e503b0258160f0f29605c64
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:38:26 +0300

    Add

---
+++ 4.Methods-Lab/03.Calculations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _03.Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case "add":
+                    Add(firstNumber,secondNumber);
+                    break;
+                case "multiply":
+                    Multiply(firstNumber,secondNumber);
+                    break;
+                case "subtract":
+                    Subtract(firstNumber,secondNumber);
+                    break;
+                case "divide":
+                    Divide(firstNumber,secondNumber);
+                    break;
+            }
+            void Add( double a, double b)
+            {
+                Console.WriteLine(a + b);
+            }
+
+            void Multiply( double a, double b)
+            {
+                Console.WriteLine(a * b);
+            }
+
+            void Subtract( double a, double b)
+            {
+                Console.WriteLine(Math.Abs(a - b));
+            }
+
+            void Divide( double a, double b)
+            {
+                Console.WriteLine(a / b);
+            }
+        }
+    }
+}
---
+++ 4.Methods-Lab/04.PrintingTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                PrintLine(1,i);
+            }
+            for (int i = number - 1; i >0 ; i--)
+            {
+                PrintLine(1, i);
+            }
+
+
+            void PrintLine(int start, int end)
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

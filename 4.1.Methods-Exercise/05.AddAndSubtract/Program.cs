a42ca7d5225d66c8b1b9786dbfc5179aacef5442
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:50:05 +0300

    Add

---
+++ 4.1.Methods-Exercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int result = Sum(first, second);
+            result = Substract(result, third);
+
+            Console.WriteLine(result);
+        }
+
+        private static int Substract(int result, int third)
+        {
+            return result - third;
+        }
+
+        private static int Sum(int first, int second)
+        {
+            return first + second;
+        }
+
+
+    }
+}
+
---
+++ 4.1.Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            IsEvenLength(str);
+            IsOddLength(str);
+        }
+
+        static void IsEvenLength(string str)
+        {
+            if (str.Length % 2 == 0)
+            {
+                int index = str.Length / 2;
+
+                for (int i = index - 1; i <= +index; i++)
+                {
+                    Console.Write(str[i]);
+                }
+            }
+        }
+        static void IsOddLength(string str)
+        {
+            if (str.Length % 2 != 0)
+            {
+                int index = str.Length / 2;
+
+                Console.Write(str[index]);
+            }
+        }
+
+    }
+}

9b572bd8f154e523eae446da6a9203b21f3fa6d5
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:16:35 +0300

    Add files via upload

---
+++ 3.Arrays-Lab/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split("").Select(int.Parse).ToArray();
+
+            int evenSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum+= numbers[i];
+                }
+            }
+            Console.WriteLine(evenSum);
+
+        }
+    }
+}

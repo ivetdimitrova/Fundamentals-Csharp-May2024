8634ca96e8a299f72ffd794e4ded9cec3c6e366d
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:13:16 +0300

    Add

---
+++ 6.1.ObjectsАndClasses-Exercise/04.Students/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace _04.Students
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                string[] information = Console.ReadLine().Split().ToArray();
+                TakeStudentInformation(students, information);
+            }
+
+         
+            students.Sort(((student, student1) => student1.Grade.CompareTo(student.Grade)));
+
+            foreach(Student student in students) 
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
+        }
+
+        private static void TakeStudentInformation(List<Student> students, string[] information)
+        {
+           Student student = new Student();
+
+           student.FirstName = information[0];
+           student.LastName = information[1];
+           student.Grade = double.Parse(information[2]);
+
+           students.Add(student);
+        }
+    }
+}

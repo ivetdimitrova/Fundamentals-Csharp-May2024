527e2e8e0b46d014fe7b6b2de70b599df2e8724d
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 19:45:34 +0300

    Add

---
+++ 4.Methods-Lab/11.Mathoperations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _11.Mathoperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculate(number1,operation,number2));
+
+        }
+
+        static int Calculate(int number, char operation, int number2)
+        {
+            int sum = 0;
+            switch (operation)
+            {
+                case '*':
+                    sum = number * number2;
+                    break;
+                case '/':
+                    sum = number / number2;
+                    break;
+                case '-':
+                    sum = Math.Abs(number - number2);
+                    break;
+                case '+':
+                    sum = number + number2;
+                    break;
+            }
+
+            return sum;
+        }
+    }
+}

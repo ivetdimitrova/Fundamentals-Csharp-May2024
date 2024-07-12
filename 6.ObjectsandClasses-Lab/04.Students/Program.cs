c8f88a6e8e021b3158961ac158a2d6e144857f1e
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 20:07:44 +0300

    Add

---
+++ 6.ObjectsandClasses-Lab/04.Students/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _04.Students
+{
+    class Student
+    {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Age { get; set; }
+    public string HomeTown { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] information = input.Split().ToArray();
+
+                TakeStudentInformation(students, information);
+            }
+
+            string city = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == city)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+
+        private static void TakeStudentInformation(List<Student> students, string[] information)
+        {
+            string firstName = information[0];
+            string lastName = information[1];
+            string age = information[2];
+            string homeTown = information[3];
+
+            Student student = new Student();
+            student.FirstName = firstName;
+            student.LastName = lastName;
+            student.Age = age;
+            student.HomeTown = homeTown;
+
+            students.Add(student);
+        }
+    }
+}
---
+++ 6.ObjectsandClasses-Lab/05.Students2.0/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace _05.Students2._0
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] information = input.Split().ToArray();
+
+                TakeStudentInformation(students, information);
+            }
+
+            string city = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == city)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+
+        private static void TakeStudentInformation(List<Student> students, string[] information)
+        {
+            string firstName = information[0];
+            string lastName = information[1];
+            string age = information[2];
+            string homeTown = information[3];
+
+            if (IsStudentExisting(students, firstName, lastName))
+            {
+                Student student = GetStudent(students, firstName, lastName);
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age = age;
+                student.HomeTown = homeTown;
+            }
+            else
+            {
+                Student student = new Student();
+                {
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.HomeTown = homeTown;
+                }
+                students.Add(student);
+            }
+        }
+
+        private static Student GetStudent(List<Student> students, string firstName, string lastName)
+        {
+            Student existingStudent = null;
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                }
+            }
+
+            return existingStudent;
+        }
+
+        private static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

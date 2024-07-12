52abf84433f4c71f6d2157fdb05e65d695da410c
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-12 18:55:57 +0300

    Add files via upload

---
+++ 2.DataTypesandVariables-Lab/05.SpecialNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int end = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= end; i++)
+            {
+                int number = i;
+                int num = i;
+            int sum = 0;
+                while(num > 0) 
+                {
+                    int digits = num % 10;
+                    num  /= 10;
+                    sum += digits;
+                }
+                switch(sum)
+                {
+                    case 5:
+                    case 7:
+                    case 11:
+                        Console.WriteLine($"{number} -> True");
+                        break;
+                    default:
+                        Console.WriteLine($"{number} -> False");
+                        break;
+
+                }
+
+            }
+        }
+    }
+}

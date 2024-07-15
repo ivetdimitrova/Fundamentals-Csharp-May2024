153a7a80b5280513cfe0dcec2368dba7c10b1555
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-15 18:26:11 +0300

    Add

---
+++ 8.1.TextProcessing-Exercise/04.CaesarCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace _04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string text = Console.ReadLine();
+
+           StringBuilder newText = new StringBuilder();
+
+           foreach (char character in text)
+           {
+               char newCharacter = (char)((int)character + 3);
+
+               newText.Append(newCharacter);
+           }
+
+           Console.WriteLine(newText.ToString());
+        }
+    }
+}
---
+++ 8.1.TextProcessing-Exercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder newText = new StringBuilder();
+            newText.Append(text[0]);
+            for (int i = 1; i < text.Length-1; i++)
+            {
+                if (text[i] == text[i + 1])
+                {
+                    continue;
+                }
+
+                newText.Append(text[i+1]);
+            }
+
+            Console.WriteLine(newText.ToString());
+        }
+    }
+}

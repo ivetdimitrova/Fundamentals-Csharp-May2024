36d838620c418f78eb29305fed5e703c5f5f9dda
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-15 18:26:55 +0300

    Add files via upload

---
+++ 8.1.TextProcessing-Exercise/07.StringExplosion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string result = ProcessExplosions(input);
+            Console.WriteLine(result);
+        }
+
+        static string ProcessExplosions(string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            int strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                    result.Append(input[i]);
+                }
+                else if (strength == 0)
+                {
+                    result.Append(input[i]);
+                }
+                else
+                {
+                    strength--;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}

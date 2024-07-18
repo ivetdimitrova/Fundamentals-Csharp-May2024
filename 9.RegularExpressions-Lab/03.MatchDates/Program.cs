0cfd18f40f68b9d00a333597c17fcb67defbeefc
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-18 16:10:01 +0300

    Add

---
+++ 9.RegularExpressions-Lab/03.MatchDates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dates = Console.ReadLine();
+
+            Regex regex = new Regex(@"\b(?<day>\d{2})(\.|-|\/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
+            MatchCollection matches = regex.Matches(dates);
+            
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+            }
+        }
+    }
+}

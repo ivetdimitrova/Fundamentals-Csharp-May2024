3312f8de1270c74545112662fb56c3f5b22e4be1
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-07-18 16:09:04 +0300

    Add

---
+++ 9.RegularExpressions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                string phones = Console.ReadLine();
+
+                var regex = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+
+                var phoneMatches = Regex.Matches(phones, regex);
+
+                var matchedPhones = phoneMatches
+                    .Cast<Match>()
+                    .Select(a => a.Value.Trim())
+                    .ToArray();
+
+                Console.WriteLine(String.Join(", ", matchedPhones));
+
+
+            }
+        
+    }
+
+}
+    
+
